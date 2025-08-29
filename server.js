edcb96534db69c5e21d8f6e3a027461020023fdd
Marco_git <m.favara2712@gmail.com>
2025-08-29 17:22:21 +0200

    update

--- server.js
+++ server.js
@@ -65,4 +65,5 @@ ${processedText}
   }
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
